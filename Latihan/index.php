a6127fdfae4b0b87acc75c9b58bb70a7c58441cf
AbdanGit <muhammadabdan918@gmail.com>
2023-06-27 17:57:53 +0800

    latihan biodata

---
+++ Latihan/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biodata Diri</title>
+</head>
+
+<body>
+
+    <h1>Isi Biodata Diri</h1>
+    <form action="" method="post">
+        <label for="nama">Nama</label>
+        <br>
+        <input type="text" name="" id="">
+        <br>
+        <label for="nama">Alamat</label>
+        <br>
+        <input type="text" name="" id="">
+        <br>
+        <label for="nama">Tempat</label>
+        <br>
+        <input type="text" name="" id="">
+        <br>
+        <label for="nama">Jenis kelamin</label>
+        <br>
+        <input type="text" name="" id="">
+        <br>
+        <label for="nama">usia</label>
+        <br>
+        <input type="text" name="" id="">
+        <br>
+    </form>
+
+</body>
+
+</html>
